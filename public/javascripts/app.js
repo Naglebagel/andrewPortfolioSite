1fb7ae0337ea602e0a4bf42dee89780c0595bbac
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-11-07 13:54:46 -0600

    changed page layout and removed obsolete js file/code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,20 +4,3 @@ $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
 });
-
-const granimInstance = new Granim({
-    element: '#canvas-radial',
-    name: 'radial-gradient',
-    direction: 'radial',
-    opacity: [1, 1],
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#ffb347', '#ffcc33'],
-                ['#83a4d4', '#b6fbff'],
-                ['#9D50BB', '#6E48AA']
-            ]
-        }
-    }
-});
